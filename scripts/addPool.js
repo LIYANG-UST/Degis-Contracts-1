621722c30686df444a7b3f571d7a38f1c8831783
liyang-iMac <ylikp@connect.ust.hk>
2021-12-06 02:48:36 +0800

    commit

--- scripts/addPool.js
+++ scripts/addPool.js
@@ -17,17 +17,17 @@ module.exports = async (callback) => {
 
     const degisPerBlock = web3.utils.toWei("1", "ether");
 
-    await farmingpool.add(fd_lptoken, degisPerBlock, false, { from: account });
+    // await farmingpool.add(fd_lptoken, degisPerBlock, false, { from: account });
 
     const BTC30000L202101 = "0x36a0952518a5bF9Edf70C406eFb238Ff699f9019";
     const ETH2000L202101 = "0xD464B7fed9c740594409969979266832d3508372";
     const AVAX30L202101 = "0xADF20fdd2026124E9a6b16481B32e249393f3e4B";
 
-    // await farmingpool.add(BTC30000L202101, degisPerBlock, false);
+    await farmingpool.add(BTC30000L202101, degisPerBlock, false);
 
-    // await farmingpool.add(ETH2000L202101, degisPerBlock, false);
+    await farmingpool.add(AVAX30L202101, degisPerBlock, true);
 
-    // await farmingpool.add(AVAX30L202101, degisPerBlock, true);
+    await farmingpool.add(ETH2000L202101, degisPerBlock, false);
 
     callback(true);
   } catch (e) {
